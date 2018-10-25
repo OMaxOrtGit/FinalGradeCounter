62abdff1b62beebae0c111e0490ea8176e52dd45
max ortman <mortman2299@stu.d214.org>
2018-10-25 14:55:04 -0500

    Initial Commit

---
+++ FinalGradeCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalGradeCounter
+//
+//  Created by max ortman on 10/25/18.
+//  Copyright © 2018 john hersey high school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
